d530dbcb11399f5a5c720c5de007478f77c340f8
Andrew Osheroff <andrewosh@gmail.com>
2024-04-04 21:44:35 +0200

    Add fastForwardTo to the ResponsePayload

--- test/basic.js
+++ test/basic.js
@@ -67,7 +67,38 @@ test('basic valid pairing with encryption key', async t => {
   candidate.handleResponse(member.response)
   const [reply] = await replied
 
-  t.alike(reply, { key, encryptionKey })
+  t.alike(reply, { key, encryptionKey, fastForwardTo: null })
+})
+
+test('basic valid pairing with encryption key and fastForwardTo', async t => {
+  t.plan(4)
+
+  const key = b4a.allocUnsafe(32).fill(1)
+  const encryptionKey = b4a.allocUnsafe(32).fill(2)
+  const fastForwardTo = {
+    key: b4a.allocUnsafe(32).fill(3),
+    length: 10
+  }
+
+  const { invite, publicKey } = createInvite(key)
+
+  const candidate = new CandidateRequest(invite, b4a.from('hello world'))
+  const member = MemberRequest.from(candidate.encode())
+
+  t.alike(member.inviteId, candidate.inviteId)
+
+  const userData = member.open(publicKey)
+  t.alike(userData, b4a.from('hello world'))
+  t.alike(member.id, candidate.id)
+
+  member.confirm({ key, encryptionKey, fastForwardTo })
+
+  const replied = once(candidate, 'accepted')
+
+  candidate.handleResponse(member.response)
+  const [reply] = await replied
+
+  t.alike(reply, { key, encryptionKey, fastForwardTo })
 })
 
 test('does not leak invitee key to unproven inviters', async t => {
@@ -133,9 +164,9 @@ test('invite response is static', async t => {
   req2.handleResponse(res2.response)
   req3.handleResponse(res3.response)
 
-  t.alike(await promise1, { key, encryptionKey })
-  t.alike(await promise2, { key, encryptionKey })
-  t.alike(await promise3, { key, encryptionKey })
+  t.alike(await promise1, { key, encryptionKey, fastForwardTo: null })
+  t.alike(await promise2, { key, encryptionKey, fastForwardTo: null })
+  t.alike(await promise3, { key, encryptionKey, fastForwardTo: null })
 })
 
 test('using a request - response', async t => {
@@ -268,5 +299,5 @@ test('candidate accepted after deny', async t => {
 
   await t.execution(accepted)
 
-  t.alike(candidate.auth, { key, encryptionKey: null })
+  t.alike(candidate.auth, { key, encryptionKey: null, fastForwardTo: null })
 })
